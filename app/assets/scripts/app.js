93ecbdf578471c2627fbfeb7ddc31eeb7ed781ad
Reuben <rcolazo89@gmail.com>
2018-09-07 20:50:32 -0700

    still figuring out how to deal with this error

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,14 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-<<<<<<< HEAD
 
 var mobileMenu = new MobileMenu();
 var revealOnScroll = new RevealOnScroll();
-=======
 import $ from 'jquery';
 
 //class^ objects below 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"), "70%");
->>>>>>> reveal-on-scroll
